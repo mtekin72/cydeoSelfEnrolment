5ea85e8625911b40f83a231961a8381bec165ff8
kapsalonadem <kapsalonadem@gmail.com>
2025-01-19 20:37:11 +0100

    Update Playwright configuration for improved parallelism and timeout settings

--- playwright.config.js
+++ playwright.config.js
@@ -1,33 +1,29 @@
 import { defineConfig, devices } from "@playwright/test";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export default defineConfig({
   testDir: "./tests",
-  fullyParallel:false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
-  reporter:'html',
+  workers: 4,
+  reporter: "html",
   projects: [
     {
-  timeout: 30 * 1000,
-  expect: {
-    timeout: 5000,
-  },
- // reporter: [["html", { outputFolder: "playwright-report" }]],
-  use: {
-    trace:'on-first-retry',
-    baseURL: process.env.BASE_URL,
-    browserName: "chromium",
-    headless: false,
-    ...devices["Desktop Chrome"],
-    viewport: { width: 1280, height: 1000 },
-    screenshot: "only-on-failure",
-    video: "retain-on-failure",
-  },
-  testMatch: ["**/*.test.js", "**/*.spec.js"],
+      timeout: 60 * 1000,
+      expect: {
+        timeout: 10000,
+      },
+      use: {
+        trace: "on-first-retry",
+        baseURL: process.env.BASE_URL,
+        browserName: "chromium",
+        headless: true,
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1280, height: 1000 },
+        screenshot: "only-on-failure",
+        video: "retain-on-failure",
+      },
+      testMatch: ["**/*.test.js", "**/*.spec.js"],
     },
   ],
 });
